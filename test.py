e0c48bc39b02efd16bcf82312e6a51be9c1ae3f7
aleksejmatankin <keklolpuck@gmail.com>
2023-05-22 20:44:27 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,15 @@
+
+print ('Hello world')
+print ('Привет мир')
+a = input('Как тебя зовут?')
+print('Привет!')
+#print(a)
+#b = input('-->')
+#print('Привет,', a)
+#print(f"Hello, {b}") #доп форматирование, лучше это использовать
+print(a, type(a))
+print(a*2, type(a))
+
+
+
+
---
+++ test2.py
@@ -0,0 +1,26 @@
+#name = str(input("Как вас зовут?"))
+#famile= str(input("Как ваша фамилия?"))
+#hello = "Привет"
+#print(hello, name, famile)
+#print(f"{hello}, {name} famile")
+import keyword
+print(*keyword.kwlist)
+print(5*3//(1-2))
+a, b = 4, 7
+if a > b:
+    print(a)
+else:
+    print(b)
+print(a == 5)
+a = 5
+print(a == 5)
+x = int(input())
+if x>0:
+    print ("positiv")
+elif x<0:
+    print("negative")
+else:
+    print("null")
+
+
+
